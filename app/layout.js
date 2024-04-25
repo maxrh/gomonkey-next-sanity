f0c8527b4c83b77ae884d08c9469290563b6f566
Max Hargreave <max@monsun.dk>
2024-04-25 04:23:09 +0200

    analytics

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import SiteHeader from "./components/ui/SiteHeader";
+import { Analytics } from "@vercel/analytics/react"
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
 				<SiteHeader />
 				{children}
 			</body>
+			<Analytics/>
 		</html>
 	);
 }
